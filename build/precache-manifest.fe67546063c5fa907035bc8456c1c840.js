d912441c283460a84587107057bf91aa11a7b6b8
Rhedil <rgomez@strongboxdata.com>
2019-03-22 15:01:11 -0600

    initial commit

---
+++ build/precache-manifest.fe67546063c5fa907035bc8456c1c840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28909fbaae58fb808a5e",
+    "url": "/static/js/main.70aa09f9.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "28909fbaae58fb808a5e",
+    "url": "/static/css/main.6c12c1ab.chunk.css"
+  },
+  {
+    "revision": "72c7fa4b9552ee1f537e53afc27b827c",
+    "url": "/index.html"
+  }
+];
